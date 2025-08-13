c89186722acf416e01e1aaae5df2920db5896053
Gerardo Albor <47321228+Alborithm@users.noreply.github.com>
2025-08-13 07:26:14 -0600

    Added logic for operations and buttons

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-console.log("Hello from JS!");
+let num1;
+let operator;
+let num2;
+
+let buttons = document.querySelectorAll("button");
+
+for( let button of buttons) {
+  button.addEventListener("click", () => console.log("ouch!"));
+}
+
+function operate(operator, num1, num2) {
+  switch (operator) {
+    case "+":
+      return add(num1, num2);
+      break;
+    case "-":
+      return subtract(num1, num2);
+      break;
+    case "x":
+      return multiply(num1, num2);
+    case "/":
+      return divide(num1, num2);
+    default:
+      return Error;
+      break;
+  }
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
