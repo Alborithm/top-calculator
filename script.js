34ddebc3f48420f353efeb82f6814505b10a35b4
Gerardo Albor <47321228+Alborithm@users.noreply.github.com>
2025-08-13 11:13:26 -0600

    Cleaned operate function

--- script.js
+++ script.js
@@ -49,17 +49,14 @@ function operate(operator, num1, num2) {
   switch (operator) {
     case "+":
       return add(num1, num2);
-      break;
     case "-":
       return subtract(num1, num2);
-      break;
     case "x":
       return multiply(num1, num2);
     case "/":
       return divide(num1, num2);
     default:
       return Error;
-      break;
   }
 }
 
